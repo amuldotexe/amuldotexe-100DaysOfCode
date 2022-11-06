9be59bfc0e18fcb13a710150922865e665b6fe4b
amuldotexe <amul.exe@gmail.com>
2022-11-06 18:52:05 +0530

    BH React Example 0001 basic functionality

--- Raw Reference Links.md
+++ Raw Reference Links.md
@@ -3,3 +3,7 @@ https://education.github.com/git-cheat-sheet-education.pdf
 
 VS Code customization
 https://stackoverflow.com/questions/42796887/switch-focus-between-editor-and-integrated-terminal
+
+
+Browserlist
+https://browserslist.dev/
---
+++ React Projects 202210/BH React Example 0001/src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM, { render } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Boski" animal="Dog" place="Durg">
+        {" "}
+      </Pet>
+      <Pet name="Nandini" animal="Cow" place="Bhilai">
+        {" "}
+      </Pet>
+      <Pet name="Gaura" animal="Cow" place="Bengaluru">
+        {" "}
+      </Pet>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
+
+// const App = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, { name: "Boski", animal: "Dog", place: "Durg" }),
+//     React.createElement(Pet, {
+//       name: "Nandini",
+//       animal: "Cow",
+//       place: "Bhilai",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Gaura",
+//       animal: "Cow",
+//       place: "Bengaluru",
+//     })
+//   );
+// };
+
+// ReactDOM.render(React.createElement(App), document.getElementById("root"));
